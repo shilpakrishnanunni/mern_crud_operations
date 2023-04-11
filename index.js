d5912513d0757e3fd418fb1e0dc3e6dc9c0f9b6d
Shilpa Krishnan Unni <shilpakrishnan@teamioss.in>
2023-04-11 12:29:12 +0530

    trying out mongoclient

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.send('hello world')
+MongoClient.connect('', (err, database) => {
+    if (err) return console.log(err)
+    app.listen(port, () => {
+        console.log('App is listening on port 3000.')
+    })
 })
 
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000,()=> {
+
+app.get('/', (req, res) => {
+    // res.send('hello world')
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    let val = req.body
+    console.log('dfjhvgbsrf', val)
+})
+
+app.listen(3000, () => {
     console.log('App is listening on port 3000.')
 })
 
